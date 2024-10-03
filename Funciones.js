ac6992e708e995667dc41fde0335342719df5907
Tavid17 <rocesar92@gmail.com>
2024-10-02 20:04:33 -0500

    carga html

---
+++ Funciones.js
@@ -0,0 +1,12 @@
+function validarRegistro(){
+    let nombreDeUsuario=document.getElementById("nombre").value; 
+    console.log(nombreDeUsuario);
+    /*let email=document.getElementById("email").value; 
+    console.log(email);
+   let number=document.getElementById("number").value; 
+    console.log(number);
+    let ciudad=document.getElementById("ciudad").value; 
+    console.log(ciudad);
+    let password=document.getElementById("password").value; 
+    console.log(password); */
+}
