ae47bddfa9971e6036eac5a421bf3d4de4f4bbe3
Karem Yeraldin Montero Ayala <Kmottf@gmail.com>
2024-10-02 20:03:59 -0500

    subir archivo

---
+++ PROYECTO/funciones.js
@@ -0,0 +1,8 @@
+function validarRegistro(){
+let NombreDeUsuario=document.getElementById("Nombre de Usuario").value;
+console.log(NombreDeUsuario);
+/*let number=document.getElementById("number");.value;
+console.log(number);*/
+}
+
+
